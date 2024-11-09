05f3908015bd7abdede821d86ea86bd8a3cd312f
ZMCodi Ubuntu <zaim.mohsin@gmail.com>
2024-11-09 18:43:29 +0000

    Fixed output for unknown MP and BP

--- element.sh
+++ element.sh
@@ -3,10 +3,41 @@ PSQL="sudo -u postgres psql --dbname=periodic_table -t --no-align -c"
 
 echo -e "\nWelcome to the periodic table database"
 
+TRY_AGAIN() {
+  echo -e "\nWould you like info on another element (y/n)? "
+  read AGAIN
+
+  case $AGAIN in
+  "y") GET_ELEMENT_INFO ;;
+  "n") exit ;;
+  *) TRY_AGAIN 
+  esac
+}
+
+PRINT_DATA() {
+  if [[ $BP_CELSIUS != "Unknown" ]]
+  then
+    echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MP_CELSIUS celsius and a boiling point of $BP_CELSIUS celsius."
+  elif [[ $MP_CELSIUS != "Unknown" ]]
+  then
+    echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MP_CELSIUS celsius but its boiling point is still unknown."
+  else
+    echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. Its melting point and boiling point is still unknown."
+  fi
+}
+
 GET_ELEMENT_INFO() {
   echo "Please enter an element (name, symbol or atomic number) to get its info:"
   read ELEMENT
 
+  if [[ -z $ELEMENT ]]
+  then
+    echo "Please enter an element"
+    GET_ELEMENT_INFO
+    return
+  fi
+
+  unset ATOMIC_NUMBER SYMBOL NAME
 
   if [[ $ELEMENT =~ ^[0-9]+$ ]]
   then
@@ -28,7 +59,7 @@ then
   else
     echo "$GET_DATA" | while IFS="|" read NAME SYMBOL TYPE ATOMIC_MASS MP_CELSIUS BP_CELSIUS
     do
-    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MP_CELSIUS celsius and a boiling point of $BP_CELSIUS celsius."
+      PRINT_DATA
     done
   fi
   
@@ -41,7 +72,7 @@ then
   else
     echo "$GET_DATA" | while IFS="|" read NAME ATOMIC_NUMBER TYPE ATOMIC_MASS MP_CELSIUS BP_CELSIUS
     do
-      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MP_CELSIUS celsius and a boiling point of $BP_CELSIUS celsius."
+      PRINT_DATA
     done
   fi
 elif [ -v NAME ]
@@ -53,7 +84,7 @@ then
   else
     echo "$GET_DATA" | while IFS="|" read SYMBOL ATOMIC_NUMBER TYPE ATOMIC_MASS MP_CELSIUS BP_CELSIUS
     do
-      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MP_CELSIUS celsius and a boiling point of $BP_CELSIUS celsius."
+      PRINT_DATA
     done
   fi
 fi
@@ -61,15 +92,5 @@ fi
 
 }
 
-TRY_AGAIN() {
-  echo "Would you like info on another element (y/n)? "
-  read AGAIN
-
-  case $AGAIN in
-  "y") GET_ELEMENT_INFO ;;
-  "n") ;;
-  *) TRY_AGAIN 
-  esac
-}
 
 GET_ELEMENT_INFO
